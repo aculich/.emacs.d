346195f7d9110f82adfe92e5d1a6383d77c2a0da
me <Aaron Culich aculich@gmail.com>
2016-07-27 08:56:33 -0400

    use recompile-on-save instead of ascmd auto-shell-command

--- init.el
+++ init.el
@@ -4007,6 +4007,7 @@ for more information about CALLBACK."
 (use-package powershell
   :defer t)
 
+;; use this instead of ascmd auto-shell-command
 (use-package recompile-on-save
   :disabled t
   :init
